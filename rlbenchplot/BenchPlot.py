58fc1512f2194f47594b467770fdadec232dbb4d
fereshteh-asg <fereshteh.asgari@gmail.com>
2022-02-15 06:36:43 +1030

    Dataframe building functions are added

--- rlbenchplot/BenchPlot.py
+++ rlbenchplot/BenchPlot.py
@@ -3,6 +3,11 @@ import numpy as np
 import pandas as pd
 from grid2op.Episode import EpisodeData
 import plotly.express as px
+import datetime
+
+from IPython.display import display
+import ipywidgets as widgets
+
 
 class BenchPlot:
     """
@@ -229,6 +234,169 @@ class BenchPlot:
 
 
 
+    def get_episode_timestamps(self):
+        """
+        This function returns a list of timestamps which associates to echa time step of the episode. 
+        NB: The len(observations) = len(actions)+1
+        """
+        observations = self.get_observations()
+        timestamp_list = []
+
+        for i in range(0,len(observations)):
+            obs=observations[i]
+            timestamp_list.append(datetime.datetime(obs.year, obs.month, obs.day, hour= obs.hour_of_day, minute= obs.minute_of_hour ))
+            
+        return timestamp_list
+
+    
+    def plot_action_freq_with_slider(self):
+        """ 
+        NB: The len(time_l) = len(data)+1
+        """
+        time_l= self.get_episode_timestamps()
+        d=self.get_actions_freq_by_type()
+        
+        action_l= [i for i in d]
+        data=pd.DataFrame.from_dict(d)
+        
+        fig = px.bar(data, x= time_l[:-1] , y=action_l).update_layout(
+            xaxis={
+                "range": [data.index, data.index.max()],
+                "rangeslider": {"visible": True},
+            })
+
+        fig.update_layout(title_text="Action Frequency per time stamp")
+
+        return fig
+    
+    
+    def create_topology_df(self):
+        c1 =0
+        c2 = 0
+        c3 = 0
+        
+        topo_df = pd.DataFrame(columns= [ 't_step', 'time_stamp', 'type',  'object_type', 'object_id', 'susbtation'])
+        for i in range(0, len(self.episode_data.actions)):
+
+            # to extract time stamp, we need the observation object:
+            obs= self.episode_data.observations[i]
+            t = datetime.datetime(obs.year, obs.month, obs.day, hour= obs.hour_of_day, minute= obs.minute_of_hour )
+
+            d= self.episode_data.actions[i].impact_on_objects()
+            if d['has_impact']:
+                c1+= len(d['topology']['disconnect_bus'])
+                c2+= len(d['topology']['bus_switch'])
+                c3+= len(d['topology']['assigned_bus'])
+                for j in d:
+                    if ( type(d[j])is dict) and (d[j]['changed']):
+                        if j== 'topology':                      
+                            if d['topology']['bus_switch']:   
+                                for n in range(0,len(d['topology']['bus_switch'])):
+                                    o_type= d['topology']['bus_switch'][n]['object_type']
+                                    o_id = d['topology']['bus_switch'][n]['object_id']
+                                    subs = d['topology']['bus_switch'][n]['substation']
+                                    topo_df.loc[len(topo_df)]= [i,t, 'swithc_bus', o_type, o_id, subs]
+
+                            if d['topology']['assigned_bus']:
+                                for n in range(0,len(d['topology']['assigned_bus'])):
+                                    #print(d['topology']['assigned_bus'][n])
+                                    print('assigned_bus')
+                                    o_type= d['topology']['assigned_bus'][n]['object_type']
+                                    o_id = d['topology']['assigned_bus'][n]['object_id']
+                                    subs = d['topology']['assigned_bus'][n]['substation']
+                                    topo_df.loc[len(topo_df)]= [i,t, 'assigned_bus', o_type, o_id, subs]
+
+                            if d['topology']['disconnect_bus']:
+                                for n in range(0,len(d['topology']['disconnect_bus'])):
+                                    o_type= d['topology']['disconnect_bus'][n]['object_type']
+                                    o_id = d['topology']['disconnect_bus'][n]['object_id']
+                                    subs = d['topology']['disconnect_bus'][n]['substation']
+                                    topo_df.loc[len(topo_df)]= [i,t,'disconnect_bus', o_type, o_id, subs]
+#         print("total Number of topology changes:" ,c1+c2+c3)                            
+        return [c1+c2+c3, topo_df]
 
 
+    def create_injection_df(self):
+        c =0
+        inj_df = pd.DataFrame(columns= [ 't_step', 'time_stamp', 'count',  'impacted'])
+        for i in range(0, len(self.episode_data.actions)):
 
+            obs=self.episode_data.observations[i]
+            t = datetime.datetime(obs.year, obs.month, obs.day, hour= obs.hour_of_day, minute= obs.minute_of_hour )
+
+            d= self.episode_data.actions[i].impact_on_objects()
+            if d['has_impact']:
+
+                for j in d:
+                    if ( type(d[j])is dict) and (d[j]['changed']):
+                        if j== 'injection':  
+                            c+= d['injection']['count']
+                            co= d['injection']['count']
+                            impacted= d['injection']['impacted']
+                            inj_df.loc[len(inj_df)]= [i,t, co, impacted]
+
+#         print('total number of injection:', c)
+        return(c, inj_df)
+
+    
+    def create_dispatch_df(self):
+        c = 0
+        dispatch_df = pd.DataFrame(columns= ['t_step','time_stamp','generator_id', 'generator_name', 'amount'])
+
+        for i in range(0, len(self.episode_data.actions)):
+
+            obs=self.episode_data.observations[i]
+            t = datetime.datetime(obs.year, obs.month, obs.day, hour= obs.hour_of_day, minute= obs.minute_of_hour )
+
+            d= self.episode_data.actions[i].impact_on_objects()
+            if d['has_impact']:      
+                for j in d:
+                    if ( type(d[j])is dict) and (d[j]['changed']):
+                        #print(d[j])
+                        if j == 'redispatch':
+                            c+=1
+                            gen= (d[j]['generators'][0])
+                            gen_id= gen['gen_id']
+                            gen_name = gen['gen_name']
+                            amount = gen['amount']
+                            #print([i, gen_id, gen_name, amount ])
+                            dispatch_df.loc[len(dispatch_df)]= [i,t, gen_id, gen_name, amount]
+#         print('total numnber of redispatches:', c)
+        return(c, dispatch_df)
+
+    
+    def present_detailed_action_type(self):
+        
+        data_display = display(display_id="data_display")
+        output_display = display(display_id="output_display")
+
+        w = widgets.Dropdown(
+            options=['Select','Tolopology changes', 'Redispatching', 'Injection', 'Switch line'],
+            value='Select',
+            description='Table', 
+        )
+        def on_change(change):
+            if change['type'] == 'change' and change['name'] == 'value':
+
+                if change['new'] =='Tolopology changes': 
+                    result = self.create_topology_df()
+                    output_display.update("total Number of topology changes:" + str(result[0]))
+                    data_display.update(result[1])
+
+                if change['new'] =='Redispatching': 
+                    result = self.create_dispatch_df()
+                    output_display.update("total Number of Redispatching changes:" + str(result[0]))
+                    data_display.update(result[1])
+
+                if change['new'] == "Injection":
+                    result = self.create_injection_df()
+                    output_display.update("total Number of Injections changes:" + str(result[0]))
+                    data_display.update(result[1])
+
+        w.observe(on_change)
+#         ouptup_display = display(display_id="ouptup_display")
+
+        display(w)
+        output_display.display('')
+        data_display.display('')
+        return
